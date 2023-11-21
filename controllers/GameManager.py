2b672d2b7d72122e8bd21d430ff2686464c1ffde
Daddy Antoine <avollet@gaming.tech>
2023-11-21 15:07:03 +0100

    GameManager: DeltaTime

---
+++ controllers/GameManager.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import *
+from PlayerManager import PlayerManager
+
+class GameManager:
+    def __init__(self):
+        pygame.init()
+        GameManager.screen = pygame.display.set_mode((1280, 720))
+        GameManager.player = PlayerManager()
+        GameManager.clock = pygame.time.Clock()
+        GameManager.deltaTime = 0
+        GameManager.running = True
+
+    def run(self):
+        while GameManager.running:
+            self.handleEvents()
+            self.update()
+            self.render()
+        pygame.quit()
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if(event.type == QUIT):
+                GameManager.running = False
+
+    def update(self):
+        GameManager.deltaTime = GameManager.clock.tick()
+
+        print(GameManager.deltaTime)
+
+    def render(self):
+        GameManager.screen.fill(Color('lime'))
+
+
+
+        pygame.display.update()
