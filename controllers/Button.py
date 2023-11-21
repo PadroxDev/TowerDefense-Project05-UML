8389d73b661170bc100c6b2a81c65da3c14d4210
Xiwang <rboisseau@gaming.tech>
2023-11-21 15:27:09 +0100

    fix button errors

--- controllers/Button.py
+++ controllers/Button.py
@@ -1,7 +1,10 @@
 import pygame
+from pygame import Rect
+from pygame.mouse import get_pos as mouse_pos
+from pygame.mouse import get_pressed as mouse_buttons
 
 class Button:
-    def __init__(self, imgPath,, dest:Rect=Rect(0, 0, 100, 100))->None:
+    def __init__(self, imgPath, dest:Rect=Rect(0, 0, 100, 100))->None:
         self.displaySurf = pygame.display.get_surface()
         self.sprite = pygame.image.load(imgPath).convert_alpha()
         self.dest = dest
@@ -12,8 +15,8 @@ class Button:
     def bind(self, to)->None:
         self.onClick = to
 
-    def update()->None:
-         if self.rect.collidepoint(mouse_pos()) and mouse_buttons()[0]:
+    def update(self)->None:
+        if self.rect.collidepoint(mouse_pos()) and mouse_buttons()[0]:
             if self.on_click and not self.previously_clicked:
                 self.previously_clicked = True
                 self.on_click()
@@ -22,7 +25,7 @@ class Button:
 
     def render(self)->None:
         if not self.displaySurf:
-            print("No surface found on button initialization"!)
+            print("No surface found on button initialization!")
             return
 
         self.rect = self.displaySurf.blit(self.sprite, self.dest)
